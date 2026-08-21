42eaaa236f6e8f30c464d0baff28eb0c129a33fb
coffeemania68 <193380009+coffeemania68@users.noreply.github.com>
2026-08-22 03:52:07 +0900

    fix(app): preserve English document language

---
+++ assets/DocumentLanguage-SWisNEWv.js
@@ -0,0 +1,1 @@
+import{r as e}from"./rolldown-runtime-S-ySWqyJ.js";import{i as t}from"./framework-CXnKph_e.js";var n=e(t(),1);function r(){return(0,n.useEffect)(()=>{document.documentElement.lang=`en`,document.documentElement.dir=`ltr`},[]),null}export{r as EnglishDocumentLanguage};
